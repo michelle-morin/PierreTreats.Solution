8501d2a02b5f1e59341971c0cffae49154788d5d
Michelle Morin <morin2m@gmail.com>
2020-03-27 08:50:29 -0700

    add models for building many-to-many relationship between treat and flavor

---
+++ Bakery/Models/Treat.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+    public class Treat
+    {
+        public Treat()
+        {
+            this.Flavors = new HashSet<FlavorTreat>();
+        }
+
+        public int TreatId { get; set; }
+        public string Name { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public ICollection<FlavorTreat> Flavors { get;}
+    }
+}
