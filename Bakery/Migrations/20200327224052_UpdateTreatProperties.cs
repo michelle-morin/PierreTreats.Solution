3576c46b05c6db9863fb4166d67e73c017ca901d
Michelle Morin <morin2m@gmail.com>
2020-03-27 15:43:37 -0700

    new migration after adding description property to treat class

---
+++ Bakery/Migrations/20200327224052_UpdateTreatProperties.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bakery.Migrations
+{
+    public partial class UpdateTreatProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Treats",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Treats");
+        }
+    }
+}
